953a68bb8f5b5e6efcf4869114f2de1821398d5a
smaug <boehoe5@gmail.com>
2023-10-15 16:54:13 +0200

    tokens: first draft of tokeniser, no error handling yet

--- tokens/minishell_tokens.c
+++ tokens/minishell_tokens.c
@@ -6,16 +6,89 @@
 /*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 14:32:40 by bvercaem          #+#    #+#             */
-/*   Updated: 2023/10/15 14:36:51 by bvercaem         ###   ########.fr       */
+/*   Updated: 2023/10/15 16:52:16 by bvercaem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_tokens.h"
 
-void	ms_tokeniser(const char *input)
+// returns the next instance of *end
+static const char	*ms_skip_quoted(const char *end)
 {
-	char	*start;
-	char	*end;
+	char	quote;
 
-	start = input;
+	quote = *end;
+	end++;
+	end = ft_strchr(end, quote);
+	if (!end)
+		exit(1);
+// error: unclosed quote
+	return (end);
+}
+
+// mallocs and adds token including start but not end
+static void	ms_add_token(const char *start, const char *end, t_darray *tokens)
+{
+	t_token	new;
+
+	if (start == end)
+		return ;
+	new.string = malloc(sizeof(char) * (end - start + 1));
+	if (!new.string)
+		exit(1);
+// error: malloc failed
+	ft_strlcpy(new.string, start, end - start + 1);
+	if (ft_darray_append(tokens, &new) == -1)
+		exit(1);
+// error: malloc failed
+}
+
+static const char	*ms_handle_symbol(char *end, t_darray *tokens)
+{
+	const char	*start;
+
+	if (!*end)
+		return (end);
+	if (ft_strchr(SKIP_SYMBOLS, *end))
+		return (end + 1);
+	start = end;
+	end++;
+	if (ft_strchr(DOUBLE_SYMBOLS, *start) && *start == *end)
+		end++;
+	ms_add_token(start, end, tokens);
+	return (end);
+}
+
+// frees all malloced token strings and calls ft_darray_delete
+void	ms_clear_tokens(t_darray *tokens)
+{
+	int	i;
+
+	i = 0;
+	while (i < tokens->size)
+	{
+		free(((t_token)((unsigned char *) tokens->contents)[i * tokens->type_size]).string);
+		i++;
+	}
+	ft_darray_delete(tokens);
+}
+
+void	ms_tokeniser(const char *input, t_darray *tokens)
+{
+	const char	*start;
+	const char	*end;
+
+	end = input;
+	while (end && *end)
+	{
+		start = end;
+		while (*end && !ft_strchr(RESERVED_SYMBOLS, *end))
+		{
+			end++;
+			if (*end == '"' || *end == '\'')
+				end = ms_skip_quoted(end);
+		}
+		ms_add_token(start, end, tokens);
+		end = ms_handle_symbol(end, tokens);
+	}
 }
--- tokens/minishell_tokens.h
+++ tokens/minishell_tokens.h
@@ -6,11 +6,22 @@
 /*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 14:38:16 by bvercaem          #+#    #+#             */
-/*   Updated: 2023/10/15 14:57:02 by bvercaem         ###   ########.fr       */
+/*   Updated: 2023/10/15 16:47:14 by bvercaem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_TOKENS_H
 # define MINISHELL_TOKENS_H
 
-# define RESERVED_SYMBOLS "|<>&"
+# include "../src/libft/libft.h"
+
+# define RESERVED_SYMBOLS "|<> "
+# define SKIP_SYMBOLS " "
+# define DOUBLE_SYMBOLS "<>"
+
+typedef struct s_token
+{
+	char	*string;
+}			t_token;
+
+#endif
