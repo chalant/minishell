db229d76045acf69395fc260f3edfffe296714ca
bvercaem <bvercaem@student.s19.be>
2023-10-25 18:31:07 +0200

    got most of the signals handled i think

--- src/minishell_main.c
+++ src/minishell_main.c
@@ -6,60 +6,91 @@
 /*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 14:00:30 by bvercaem          #+#    #+#             */
-/*   Updated: 2023/10/24 19:12:28 by bvercaem         ###   ########.fr       */
+/*   Updated: 2023/10/25 18:24:08 by bvercaem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ms_exit(void)
+pid_t	pid;
+
+void	ms_exit(int exit_value)
 {
 	write(STDOUT_FILENO, "exit\n", 5);
 	rl_clear_history();
-	exit(0);
+	exit(exit_value);
 }
 
-// should free input
-int	ms_process_input(char *input)
+// should free line
+int	ms_process_line(char *line)
 {
-	if (!ft_strncmp(input, "exit", 5))
+	if (!ft_strncmp(line, "exit", 5))
 	{
-		free(input);
-		ms_exit();
+		free(line);
+		ms_exit(0);
+	}
+	if (!ft_strncmp(line, "sleep", 6))
+	{
+		pid = fork ();
+		if (!pid)
+		{
+			free (line);
+			rl_clear_history();
+			execve ("sleep", NULL, NULL);
+			exit (1);
+		}
+		if (pid)
+			waitpid(pid, NULL, 0);
+		pid = 0;
 	}
 // do more stuff :)
-if (!ft_strncmp(input, "re", 3))
-{
-}
-write(1, input, ft_strlen(input));
-write(1, "\n", 1);
-	free(input);
+	if (*line)
+		add_history(line);
+	free(line);
 	return (0);
 }
 
 void	ms_new_prompt(int sig)
 {
-	(void)sig;
+	(void) sig;
 	write(STDOUT_FILENO, "\n", 1);
+	if (pid)
+		return ;
 	rl_replace_line("", 1);
 	rl_on_new_line();
 	rl_redisplay();
 }
 
-// C-d (EOF) with no text prints "exit\n" and exits
+void	ms_kill_pid(int sig)
+{
+	(void) sig;
+	if (pid)
+	{
+		kill (pid, SIGQUIT);
+		write (STDOUT_FILENO, "Quit: 3\n", 8);
+	}
+}
+
 int	main(void)
 {
-	char	*input;
-	struct sigaction	act;
+	char				*line;
+	extern int			rl_catch_signals;
+	struct sigaction	act_sigint;
+	struct sigaction	act_sigquit;
 
-	act.__sigaction_u.__sa_handler = ms_new_prompt;
-	sigaction(SIGINT, &act, NULL);
-	input = readline(MS_PROMPT);
-	while (input)
+	pid = 0;
+	rl_catch_signals = 0;
+	ft_bzero(&act_sigint, sizeof(struct sigaction));
+	act_sigint.__sigaction_u.__sa_handler = ms_new_prompt;
+	sigaction(SIGINT, &act_sigint, NULL);
+	ft_bzero(&act_sigquit, sizeof(struct sigaction));
+	act_sigquit.__sigaction_u.__sa_handler = ms_kill_pid;
+	sigaction(SIGQUIT, &act_sigquit, NULL);
+	line = readline(MS_PROMPT);
+	while (line)
 	{
-		ms_process_input(input);
-		input = readline(MS_PROMPT);
+		ms_process_line(line);
+		line = readline(MS_PROMPT);
 	}
-// try to get rid of the newline?
-	ms_exit();
+	ms_exit(0);
 }
