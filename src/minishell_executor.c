76a1455fed90d64ea71cd3b585798b4ca77ebb56
chalant <chalantvsy@gmail.com>
2023-11-15 12:39:56 +0100

    executor

--- src/minishell_executor.c
+++ src/minishell_executor.c
@@ -1,79 +1,19 @@
 #include "minishell.h"
 
-<<<<<<< Updated upstream
-//executes the right command when the left command succeedes
-=======
->>>>>>> Stashed changes
 int	execute_and(t_command *command, int in_pipe[2], int out_pipe[2])
 {
 	int	status;
 
-<<<<<<< Updated upstream
-	status = execute_command(command->left, in_pipe, out_pipe);
-	if (status == 0)
-		return	(execute_command(command->right, in_pipe, out_pipe));
-	return (status);
-}
-
-//executes right command when the left commands fails.
-=======
 	status = execute_command(command, in_pipe, out_pipe);
 	if (status == 0)
 		return (execute_command(command, in_pipe, out_pipe));
 	return (status);
 }
 
->>>>>>> Stashed changes
 int	execute_or(t_command *command, int in_pipe[2], int out_pipe[2])
 {
 	int	status;
 
-<<<<<<< Updated upstream
-	status = execute_command(command->left, in_pipe, out_pipe);
-	if (status > 0)
-		return (execute_command(command->right, in_pipe, out_pipe));
-	return (status);
-}
-
-int	execute_pipe(t_command *command, int in_pipe[2], int out_pipe[2])
-{
-	int	status;
-	int	pid1;
-	int	pid2;
-
-	status = execute_command(command->left, in_pipe, out_pipe);
-	pid1 = fork();
-	if (pid1 == 0)
-		execute_command(command->right, in_pipe, out_pipe);
-	pid1 = fork();
-	if (pid2 == 0)
-		execute_command(command->r);
-	return (status)
-}
-
-int	execute_simple_command(t_command *command, int in_pipe[2], int out_pipe[2])
-{
-	printf("Executed! %s\n", command->command_name);
-	return (1);
-}
-
-int	execute_command(t_command *command, int in_pipe[2], int out_pipe[2])
-{
-	if (!command->command_name)
-		return (0);
-	//printf("Executing %s\n", command->command_name);
-	if (command->command_flags & MS_OPERAND)
-		return (execute_simple_command(command, in_pipe, out_pipe));
-	else if (command->command_flags & MS_OPERATOR)
-	{
-		if (command->command_flags & MS_AND)
-			return (execute_and(command, in_pipe, out_pipe));
-		else if (command->command_flags & MS_OR)
-			return (execute_or(command, in_pipe, out_pipe));
-		else if (command->command_flags & MS_PIPE)
-			return (execute_pipe(command, in_pipe, out_pipe));
-	}
-=======
 	status = execute_command(command, in_pipe, out_pipe);
 	if (status > 0)
 		return (execute_command(command, in_pipe, out_pipe));
@@ -117,7 +57,6 @@ int	execute_command(t_command *command, int in_pipe[2], int out_pipe[2])
 	else if (command->command_flags & MS_AND)
 		return (execute_and(command, in_pipe, out_pipe));
 	else if (command->command_flags & MS_OR)
->>>>>>> Stashed changes
 	return (1);
 }
 
