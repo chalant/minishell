4131a53ba1c77586456dea9a4d518abd1282c39c
chalant <chalantvsy@gmail.com>
2024-09-03 21:40:16 +0200

    fixed hanging for < valid_file || cat

--- src/minishell_execution_operators.c
+++ src/minishell_execution_operators.c
@@ -63,7 +63,10 @@ int	execute_or(t_command *parent, t_command *command,
 	{
 		distribute_fds(command, command->right);
 		if (command->command_flags & MS_OPERATOR && !command->left)
-			handle_redirections(command->right, open_prepend);
+		{
+			if (handle_redirections(command->right, open_prepend))
+				return (0);
+		}
 		return (execute_command(command, command->right, in_pipe, out_pipe));
 	}
 	return (status);
